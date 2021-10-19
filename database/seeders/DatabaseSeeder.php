45cffe8027a899dfa3dbe3fae3bfecd0c8a11a27
ryo <curo0709@gmail.com>
2021-10-19 14:43:10 +0900

    message

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(GenresTableSeeder::class);
         $this->call(AreasTableSeeder::class);
-        $this->call(ShopsTableSeeder::class);
+        // $this->call(ShopsTableSeeder::class);
     }
 }
