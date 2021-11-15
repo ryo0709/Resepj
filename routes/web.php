7dde8169c66e85b29998bf86d26d44e0051f8a7c
ryo <curo0709@gmail.com>
2021-11-15 10:19:27 +0900

    11_15fix

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [ShopController::class, 'index'])->name('index');//トップページ　飲食店一覧ページ
+Route::get('/detail/{shop_id}', [ShopController::class, 'detail'])->name('detail');//飲食店詳細ページ
 
-Route::get('/mypage', [UserController::class, 'mypage']);
-Route::get('/thanks', function () {
+Route::get('/mypage', [UserController::class, 'mypage']);//マイページ
+Route::get('/thanks', function () {//サンクスページ　（会員登録完了）
     return view('thanks');
 });
 
-Route::get('/', [ShopController::class, 'index'])->name('index');
-Route::get('/detail/{shop_id}', [ShopController::class, 'detail'])->name('detail');
+Route::get('/area_search', [ShopController::class, 'area_search']);//エリア検索機能
+Route::get('/name_search', [ShopController::class, 'name_search']);//店舗名検索機能　＊ジャンル検索はｊｓ
 
+Route::get('/liked', [LikeController::class, 'liked'])->name('liked');//Like機能　お気に入り店舗追加・削除
 
-Route::get('/area_search', [ShopController::class, 'area_search']);
-Route::get('/name_search', [ShopController::class, 'name_search']);
-
-Route::get('/liked', [LikeController::class, 'liked'])->name('liked');
-
-Route::post('/reservation', [ReservationController::class, 'reservation']);
-Route::post('/reservation_change', [ReservationController::class, 'reservation_change']);
-Route::post('/delete/{resevation_id}', [ReservationController::class, 'delete'])->name('delete');
-Route::get('/done', function () {
+Route::post('/reservation', [ReservationController::class, 'reservation']);//予約機能
+Route::post('/reservation_change', [ReservationController::class, 'reservation_change']);//予約変更機能
+Route::post('/delete/{resevation_id}', [ReservationController::class, 'delete'])->name('delete');//予約削除機能
+Route::get('/done', function () {//予約完了ページ
     return view('done');
 });
-Route::post('/review', [ReviewController::class, 'review']);
-Route::post('/review_change', [ReviewController::class, 'review_change']);
 
+Route::post('/review', [ReviewController::class, 'review']);//評価機能　（レビュー）
+Route::post('/review_change', [ReviewController::class, 'review_change']);//評価変更機能
 
 Route::get('/dashboard', function () {
     return view('dashboard');
